4521362858fc08829baa693954e16a8a3726d06f
Jason Zutterling <jason.zutterling@gmail.com>
2025-04-02 15:09:58 -0400

    Update to default to event 2025ohcl

--- 2025/reefscape_config.js
+++ 2025/reefscape_config.js
@@ -15,7 +15,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2025nyro",
+      "defaultValue": "2025ohcl",
       "required": "true"
     },
     { "name": "Match Level",
